fe633ace06d86147ce4375c2b777b0bb101166f2
Auzan <istikhoriauzan@gmail.com>
2024-08-31 21:36:46 +0700

    init

---
+++ SampleProject.API/Customers/CustomersController.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SampleProject.Application.Customers;
+using SampleProject.Application.Customers.GetCustomerDetails;
+using SampleProject.Application.Customers.RegisterCustomer;
+
+namespace SampleProject.API.Customers
+{
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomersController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public CustomersController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        /// <summary>
+        /// Register customer.
+        /// </summary>
+        [Route("")]
+        [HttpPost]
+        [ProducesResponseType(typeof(CustomerDto), (int)HttpStatusCode.Created)]
+        public async Task<IActionResult> RegisterCustomer([FromBody]RegisterCustomerRequest request)
+        {
+           var customer = await _mediator.Send(new RegisterCustomerCommand(request.Email, request.Name));
+
+           return Created(string.Empty, customer);
+        }
+
+        //create a new endpoint for getting customer details
+        /// <summary>
+        /// get customer details.
+        /// </summary>
+        [Route("{customerId}")]
+        [HttpGet]
+        [ProducesResponseType(typeof(CustomerDto), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetCustomerDetails([FromRoute] CustomerDetailsDto customerId)
+        {
+            var customer = await _mediator.Send(new CustomerDetailsDto(customerId));
+
+            return Ok(customer);
+        }
+    }
+}
